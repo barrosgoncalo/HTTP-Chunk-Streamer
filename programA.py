8343b82cb608af2b4e7808a87029f127e089e1be
barrosgoncalo <barrosgoncalobarros@gmail.com>
2025-11-29 20:18:10 +0000

    fix: program B refactor

--- programA.py
+++ programA.py
@@ -2,62 +2,65 @@ import os
 from socket import *
 from sys import *
 
-# python3 programA.py serverURL movieName resultsFileName
-# ARGS: programA.py serverURL movieName resultsFileName
+def arg_validation():
+    if len(argv) != 4:
+        print("Error: wrong number of arguments")
+        exit(1)
 
-buffer_size = 1024
+BUFFER_SIZE = 1024
 
-if len(argv) != 4:
-    print("Error: wrong number of arguments")
-    exit(1)
+def main():
+    if len(argv) != 4:
+        print("Error: wrong number of arguments")
+        exit(1)
 
-url = argv[1]
-movie_name = argv[2]
-results_file_name = argv[3]
+    url = argv[1]
+    movie_name = argv[2]
+    results_file_name = argv[3]
 
-addr, port = (url.split('/')[2]).split(':')
+    addr, port = (url.split('/')[2]).split(':')
 
-socket = socket(family=AF_INET, type=SOCK_STREAM)
-socket.connect((addr, int(port)))
+    sck = socket(family=AF_INET, type=SOCK_STREAM)
+    sck.connect((addr, int(port)))
 
-request = f'GET ./{movie_name}/manifest.txt HTTP/1.0\r\n\r\n'
+    request = f'GET ./{movie_name}/manifest.txt HTTP/1.0\r\n\r\n'
 
-socket.send(request.encode())
+    sck.send(request.encode())
 
-rcvd_data = socket.recv(buffer_size)
+    rcvd_data = sck.recv(BUFFER_SIZE)
 
-# manifest.txt file download
+    # manifest.txt file download
 
-tmp = b'' # tmp variable
+    tmp = b'' # tmp variable
 
-while rcvd_data:
-    tmp += rcvd_data
-    rcvd_data = socket.recv(buffer_size)
+    while rcvd_data:
+        tmp += rcvd_data
+        rcvd_data = sck.recv(BUFFER_SIZE)
 
-tmp_str = tmp.decode()
-header_end = tmp_str.find("\r\n\r\n")
+    tmp_str = tmp.decode()
+    header_end = tmp_str.find("\r\n\r\n")
 
-if header_end == -1:
-    print("Invalid HTTP")
-    exit(1)
+    if header_end == -1:
+        print("Invalid HTTP")
+        exit(1)
 
-content = tmp_str[header_end + 4:]
-lines = content.split("\n")
+    content = tmp_str[header_end + 4:]
+    lines = content.split("\n")
 
-with open(results_file_name, "w") as rf:
-    num_tracks = int(lines[1])
-    num_seg = int(lines[6])
-    rf.write(f"{num_tracks}\n{num_seg}\n")
-    total = 0
-    movie_header = 2
-    track_header = 5
-    offset = movie_header + track_header
+    with open(results_file_name, "w") as rf:
+        num_tracks = int(lines[1])
+        num_seg = int(lines[6])
+        rf.write(f"{num_tracks}\n{num_seg}\n")
+        movie_header = 2
+        track_header = 5
+        offset = movie_header + track_header
 
-    for t in range(num_tracks):
-        total = 0
-        for s in range(offset, offset + num_seg - 1):
-            total += int(lines[s].split(" ")[1])
-        rf.write(f"{total}\n")
-        offset += track_header + num_seg
+        for t in range(num_tracks):
+            total = 0
+            for s in range(offset, offset + num_seg - 1):
+                total += int(lines[s].split(" ")[1])
+            rf.write(f"{total}\n")
+            offset += track_header + num_seg
 
-
+if __name__ == "__main__":
+    main()
--- programB.py
+++ programB.py
@@ -1,77 +1,113 @@
 import time
 from socket import *
 from sys import  *
+
 import requests
 
-from programA import buffer_size, offset
+BUFFER_SIZE = 1024
 
-if len(argv) != 4:
-    print("Error: wrong number of arguments")
-    exit(1)
+MOVIE_HEADER_SIZE = 2
+TRACK_HEADER_SIZE = 5
 
-url = argv[1]
-movie_name = argv[2]
-results_file_name = argv[3]
+def arg_validation():
+    if len(argv) != 4:
+        print("Error: wrong number of arguments")
+        exit(1)
 
-addr, port = (url.split('/')[2]).split(':')
-
-socket = socket(family=AF_INET, type=SOCK_STREAM)
-socket.connect((addr, int(port)))
-
-request = f"GET ./{movie_name}/manifest.txt HTTP/1.0\r\n\r\n"
-
-socket.send(request.encode())
-
-recv_data = socket.recv(buffer_size)
-
-tmp = b''
-
-while recv_data:
-    tmp += recv_data
-    recv_data = socket.recv(buffer_size)
-
-tmp_str = tmp.decode()
-header_end = tmp_str.find("\r\n\r\n")
-
-if header_end == -1:
-    print("Invalid HTTP")
-    exit(1)
-
-content = tmp_str[header_end + 4:]
-lines = content.split("\n")
-
-with open(results_file_name, "w") as rf:
-    num_tracks = int(lines[1])
-    num_seg = int(lines[6])
-
-    movie_header = 2
-    track_header = 5
-    offset = movie_header + track_header
-    track_name_line = movie_header
-
-    for t in range(num_tracks):
-        track_name = lines[track_name_line]
-        url = f'http://{addr}:{port}/{movie_name}/{track_name}' # request url
-        print(f'{url}\n')
-        duration = 0
-        for s in range(offset, offset + num_seg):
-            seg = lines[s]
-            seg_offset, seg_size = map(int, seg.split(" "))
-            seg_end = seg_size + seg_offset - 1
-            headers = {"Range" : f"bytes={seg_offset}-{seg_end}"} # request header
-            start = time.time()
-            # download
-            video_data = requests.get(url, headers=headers)  # request
-            end = time.time()
-            duration += end - start
-
-        rf.write(f'{str(duration)}\n')
-        offset += track_header + num_seg # offset update
-        track_name_line += num_seg + track_header
+def parse_url(url):
+    host, port = (url.split('/')[2]).split(':')
+    return host, int(port)
 
 
+def send_manifest_request(sck, movie_name):
+    # send manifest request
+    request = f"GET ./{movie_name}/manifest.txt HTTP/1.0\r\n\r\n"
+    sck.send(request.encode())
+
+def read_full_response(sck):
+    recv_data = sck.recv(BUFFER_SIZE)
+    data = b''
+    while recv_data:
+        data += recv_data
+        recv_data = sck.recv(BUFFER_SIZE)
+    return data
+
+
+def extract_http_body(response):
+    header_end = response.find("\r\n\r\n")
+    if header_end == -1:
+        print("Invalid HTTP")
+        exit(1)
+    return response[header_end + 4:]
+
+def build_request_url(url, movie_name, track_name):
+    host, port = parse_url(url)
+    return f'http://{host}:{port}/{movie_name}/{track_name}'
+
+def download_track(segment_start_idx, segment_end_idx, manifest_lines, url):
+    start = time.time()
+    size = download(segment_start_idx, segment_end_idx, manifest_lines, url)
+    duration = time.time() - start
+    return duration, size
+
+def download(segment_start_idx, segment_end_idx, manifest_lines, url):
+    total_size = 0
+    for i in range(segment_start_idx, segment_end_idx):
+        offset_str, size_str = manifest_lines[i].split()
+        offset, size = int(offset_str), int(size_str)
+        byte_end = size + offset - 1
+        headers = {"Range": f"bytes={offset}-{byte_end}"}  # request header
+        video_data = requests.get(url, headers=headers) # segment download
+        total_size += len(video_data.content)
+    return total_size
+
+
+def result_file_maker(results_file_name, lines, url, movie_name):
+    with open(results_file_name, "w") as rf:
+        num_tracks = int(lines[1])
+        num_seg = int(lines[6])
+        offset = MOVIE_HEADER_SIZE + TRACK_HEADER_SIZE
+        track_name_line = MOVIE_HEADER_SIZE
+
+        for _ in range(num_tracks):
+            download_time, download_rate = download_track(
+                offset,
+                offset + num_seg,
+                lines,
+                build_request_url(url, movie_name, lines[track_name_line]) # request url,
+            )
+            rf.write(f'{str(download_time)}\n{str(download_rate)}\n')
+
+            # move to next track block
+            offset += TRACK_HEADER_SIZE + num_seg
+            track_name_line += num_seg + TRACK_HEADER_SIZE
 
 
 
-# print(content)
+
+def main():
+
+    arg_validation()
+
+    url = argv[1]
+    movie_name = argv[2]
+    results_file_name = argv[3]
+
+    with socket(family=AF_INET, type=SOCK_STREAM) as sck:
+        # connect to socket
+        host, port = parse_url(url)
+        sck.connect((host, int(port)))
+        # REQUEST for manifest file
+        send_manifest_request(sck, movie_name)
+        # RECEIVAL of manifest file
+        raw_response = read_full_response(sck).decode()
+
+    # extract data in lines
+    lines = extract_http_body(raw_response).split("\n")
+
+    result_file_maker(results_file_name, lines, url, movie_name)
+
+
+if __name__ == "__main__":
+    main()
 
